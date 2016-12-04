9eef499789fa993569d4f2365e56f047f1443fe9
benediktreinke <reinkebenedikt@gmail.com>
2016-12-04 14:34:26 +0000

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+function nextmov(){
+	document.getElementById("")
+}
+
+function reply_click(clicked_id)
+{
+    //alert(clicked_id);
+    var idd = "container" + clicked_id;
+    //alert(idd);
+    document.getElementById(idd).style.display = "none"; 
+    
+    var ran = "container" + Math.floor((Math.random() * 5) + 1);
+    alert(ran);
+    document.getElementById(ran).style.display = "block";
+}
